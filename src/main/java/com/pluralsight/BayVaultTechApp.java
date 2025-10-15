9ce8b250a58672dea4d3f87a5bfcfc7e99d204d7
Alex Diaz(He/Hiim)(Gilroy) <alexander.adolfo.diaz017@gmail.com>
2025-10-15 12:01:10 -0700

    Adding notes to my "BayVaultTech.java(Start Button)" to help explain later on during presentation.

--- src/main/java/com/pluralsight/BayVaultTechApp.java
+++ src/main/java/com/pluralsight/BayVaultTechApp.java
@@ -1,6 +1,5 @@
 package com.pluralsight;
 
-
 import java.awt.*;
 
 public class BayVaultTechApp {
@@ -9,3 +8,7 @@ public class BayVaultTechApp {
         homeScreen.showMenu();
     }
 }
+//Notes for presentation:
+//1.this is basically my "Start Screen"
+//2.it creates the start screen & shows the menu
+//3.try to think about as: it turns on your menu screen so everything else can happen after
